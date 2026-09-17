55b77b979107d5373c00e71bf832da06e0cf3fa0
Palak Goyal <124762359+palakgoyal2015@users.noreply.github.com>
2026-09-17 15:43:58 +0530

    Auto-save LinkedList.java

--- java/LinkedList.java
+++ java/LinkedList.java
@@ -1,80 +1,45 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
-class ListNode{
-    int val;
-    ListNode next;
-    ListNode(int val){
-        this.val=val;
+         ListNode head = new ListNode(10);
+        ListNode second = new ListNode(20);
+        ListNode third = new ListNode(30);
+        ListNode fourth = new ListNode(40);
+        ListNode fivth = new ListNode(10);
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next=fivth;
+        // fivth.next=second;
+        // boolean ans=isPalindrome(head);
+        // System.out.print(ans);
+            // First sorted list
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(3);
+        list1.next.next = new ListNode(5);
+
+
+        // Second sorted list
+        ListNode list2 = new ListNode(2);
+        list2.next = new ListNode(4);
+        list2.next.next = new ListNode(6);
+
+
+        // Merge both lists
+        head = mergeTwoLists(list1, list2);
+
+
+        // Print merged list
+        printList(head);
+        // boolean ans=hasCycle(head);
+        // System.out.print(ans);
+        // ListNode middle=middleLinkedList(head);
+        // System.out.print(middle.val);
+        // head=deleteLastNode(head);
+        // printList(head);
+        // insertAtEnd(head,1);
+        // printList(head);
+        // boolean ans=isPresent(head,50);
+        // System.out.print(ans);
+        // printList(head);
+        // int ans=length(head);
+        // System.out.print(ans);
     }
-
-
 }
-class Main {
-    static void printList(ListNode head){
-    ListNode current=head;
-    while(current!=null){
-        System.out.print(current.val+" ");
-        current=current.next;
-    }
-    }
-    static int length(ListNode head){
-        int count=0;
-        ListNode current=head;
-        while(current!=null){
-            count++;
-            current=current.next;
-        }
-        return count;
-    }
-    static boolean isPresent(ListNode head,int k){
-        ListNode current=head;
-        while(current!=null){
-            if(current.val==k){
-                return true;
-            }
-            current=current.next;
-        }
-        return false;
-    }
-    static ListNode insertAtBeginning(ListNode head,int k){
-        ListNode newNode=new ListNode(k);
-        newNode.next=head;
-        return newNode;
-        
-    }
-    static void insertAtEnd(ListNode head,int k){
-        ListNode newNode=new ListNode(k);
-        if(head==null){
-            head=newNode;
-            return;
-        }
-        ListNode current=head;
-        while(current.next!=null){
-            current=current.next;
-        }
-        current.next=newNode;
-    }
-    static ListNode deleteFirstNode(ListNode head){
-        if(head==null){
-            return null;
-        }
-        head=head.next;
-        return head;
-    }
-    static ListNode deleteLastNode(ListNode head){
-        if(head==null||head.next==null){
-            return null;
-        }
-        ListNode current=head;
-        while(current.next.next!=null){
-            current=current.next;
-        }
-        current.next=null;
-        return head;
-    }
-    static ListNode reverseLinkedList(ListNode head){
-        ListNode prev=null;
-        ListNode current=head;
-        while(current!=null){
-            ListNode nnext=current.next;
-            current.next=prev;
